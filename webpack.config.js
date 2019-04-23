9e0483dcbf5fb2a72601fc2871bd513206cb3a30
MAZux <mazen270@hotmail.com>
2019-04-23 21:36:38 +0300

    Revert "Too Many Redrect :/"
    This reverts commit 3e9629c9a8cd56159c9cb13b256724c29d54f7a3.

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('back', './assets/js/back.js')
-    .addEntry('front', './assets/js/front.js')
+    .addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -49,7 +48,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    .enableSassLoader()
+    //.enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
