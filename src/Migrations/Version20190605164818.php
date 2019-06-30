9a69881d41550ecfa5b4acc8957857bbc12149a9
MAZux <mazen270@hotmail.com>
2019-06-30 22:09:19 +0300

    New Migrations

---
+++ src/Migrations/Version20190605164818.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190605164818 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE blog DROP FOREIGN KEY FK_C01551439777D11E');
+        $this->addSql('DROP INDEX IDX_C01551439777D11E ON blog');
+        $this->addSql('ALTER TABLE blog CHANGE category_id_id category_id INT NOT NULL');
+        $this->addSql('ALTER TABLE blog ADD CONSTRAINT FK_C015514312469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_C015514312469DE2 ON blog (category_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE blog DROP FOREIGN KEY FK_C015514312469DE2');
+        $this->addSql('DROP INDEX IDX_C015514312469DE2 ON blog');
+        $this->addSql('ALTER TABLE blog CHANGE category_id category_id_id INT NOT NULL');
+        $this->addSql('ALTER TABLE blog ADD CONSTRAINT FK_C01551439777D11E FOREIGN KEY (category_id_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_C01551439777D11E ON blog (category_id_id)');
+    }
+}
